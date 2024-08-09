468ea5055a01a8a51ad332219ee8ffb7f35c14c8
stiven trujillo <strujillodev@gmail.com>
2024-08-09 10:11:31 -0500

    feat: create update game

--- src/controllers/GameController.ts
+++ src/controllers/GameController.ts
@@ -13,21 +13,22 @@ export class GameController {
       res.status(500).send("Error creating game");
     }
   };
-
+  
   static getAllGames = async (req: Request, res: Response) => {
     try {
       const games = await Game.find({});
       res.json(games);
     } catch (error) {
       console.log(error);
+      res.status(500).send("Error getting games");
     }
   };
-
+  
   static getGameById = async (req: Request, res: Response) => {
     const { id } = req.params;
     try {
       const game = await Game.findById(id);
-
+      
       if (!game) {
         const error = new Error("Game not found");
         return res.status(404).json({ error: error.message });
@@ -35,6 +36,25 @@ export class GameController {
       res.json(game);
     } catch (error) {
       console.log(error);
+      res.status(500).send("Error getting game by id");
+    }
+  };
+  
+  static updateGame = async (req: Request, res: Response) => {
+    const { id } = req.params;
+    try {
+      const game = await Game.findByIdAndUpdate(id, req.body);
+
+      if (!game) {
+        const error = new Error("Game not found");
+        return res.status(404).json({ error: error.message });
+      }
+      
+      await game.save();
+      res.send("Game updated successfully");
+    } catch (error) {
+      console.log(error);
+      res.status(500).send("Error updating game");
     }
   };
 }
--- src/routes/GameRoutes.ts
+++ src/routes/GameRoutes.ts
@@ -26,4 +26,27 @@ router.get(
   GameController.getGameById
 );
 
+router.put(
+  "/:id",
+  param("id").isMongoId().withMessage("Invalid Game ID"),
+  body("gameName")
+    .isString()
+    .isLength({ min: 3 })
+    .notEmpty()
+    .withMessage("Game Name is required"),
+  body("date").notEmpty().withMessage("Date is required"),
+  body("players")
+    .notEmpty()
+    .withMessage("Players is required")
+    .isArray()
+    .withMessage("Players must be an array"),
+  body("numbersDrawn")
+    .notEmpty()
+    .withMessage("numbersDrawn is required")
+    .isArray()
+    .withMessage("numbersDrawn must be an array"),
+  handleInputErrors,
+  GameController.updateGame
+);
+
 export default router;
