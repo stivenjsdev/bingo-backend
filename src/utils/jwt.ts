6757584d93ee8814275b62c2c52dcfbcac8fd2c1
stiven trujillo <strujillodev@gmail.com>
2024-09-13 14:14:25 -0500

    feat: change expire time

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -6,7 +6,7 @@ type UserPayload = {
 };
 
 export const generateJWT = (payload: UserPayload) => {
-  const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: "180m" });
+  const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: "480m" });
 
   return token;
 };
